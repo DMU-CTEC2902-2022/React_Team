89e24f304b99349030f9177947b3617464f94b1f
Vladimirs <vladimirribakov123@mail.ru>
2022-02-17 09:56:41 +0000

    Fully changed to MVC Full Solution

---
+++ InformationandTechnologyCollege/InformationandTechnologyCollege/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InformationandTechnologyCollege.MvcApplication" Language="C#" %>
---
+++ InformationandTechnologyCollege/InformationandTechnologyCollege/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InformationandTechnologyCollege.Startup))]
+namespace InformationandTechnologyCollege
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
