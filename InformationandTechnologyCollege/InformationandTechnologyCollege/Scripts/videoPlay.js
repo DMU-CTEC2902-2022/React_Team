72e5a063005b893d4fe6135c9e0ab52a809c8187
P2601563 <P2601563@CLPX0013WIN021W>
2022-03-17 10:57:29 +0000

    uploaded video on the about page

---
+++ InformationandTechnologyCollege/InformationandTechnologyCollege/Scripts/videoPlay.js
@@ -0,0 +1,51 @@
+﻿var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-placeholder', {
+        width: 600,
+        height: 400,
+        videoId: '3vLDiCgTFpY',
+        playerVars: {
+            color: 'white',
+            playlist: 'taJ60kskkns,FG0fTKAqZ5g'
+        },
+        events: {
+            onReady: initialize
+        }
+    });
+}
+function initialize() {
+
+    // Update the controls on load
+    updateTimerDisplay();
+    updateProgressBar();
+
+    // Clear any old interval.
+    clearInterval(time_update_interval);
+
+    // Start interval to update elapsed time display and
+    // the elapsed part of the progress bar every second.
+    time_update_interval = setInterval(function () {
+        updateTimerDisplay();
+        updateProgressBar();
+    }, 1000)
+
+}
+
+// This function is called by initialize()
+function updateTimerDisplay() {
+    // Update current time text display.
+    $('#current-time').text(formatTime(player.getCurrentTime()));
+    $('#duration').text(formatTime(player.getDuration()));
+}
+
+function formatTime(time) {
+    time = Math.round(time);
+
+    var minutes = Math.floor(time / 60),
+        seconds = time - minutes * 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    return minutes + ":" + seconds;
+}
