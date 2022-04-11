9376008f739db63584ffa31592d1845151d551da
Vladimirs <vladimirribakov123@mail.ru>
2022-04-11 22:08:15 +0100

    start working on the testing

---
+++ InformationandTechnologyCollege/InformationandTechnologyCollege.Tests/Controllers/CourseTesting.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using InformationandTechnologyCollege.Models;
+using InformationandTechnologyCollege.Controllers;
+
+namespace InformationandTechnologyCollege.Tests.Controllers
+{
+    [TestClass]
+    public class CourseTesting
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            CourseController AllCourses = new CourseController();
+            //test to see if it exists
+            Assert.IsNotNull(AllCourses);
+        }
+    }
+}
