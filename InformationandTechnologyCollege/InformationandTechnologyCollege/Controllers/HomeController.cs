44d4e352f1b64381d15b314e1dc313ecf0a56994
Vladimirs <vladimirribakov123@mail.ru>
2022-04-12 16:45:15 +0100

    added comments and some design to the register page

--- InformationandTechnologyCollege/InformationandTechnologyCollege/Controllers/HomeController.cs
+++ InformationandTechnologyCollege/InformationandTechnologyCollege/Controllers/HomeController.cs
@@ -10,19 +10,21 @@ namespace InformationandTechnologyCollege.Controllers
     {
         public ActionResult Index()
         {
+            //return view for main page
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "The College of West Anglia is focused on developing your skills and talents to prepare you for work or university";
-
+            //return view for about page and create a message to be populated on the page
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+            //return view for contact page and create a message to be populated on the page
 
             return View();
         }
