3959f5b6019a796eb1fd2d0c4da9fd83f7b94d06
Reegan Hodges <P2598200@my365.dmu.ac.uk>
2022-04-13 16:10:29 +0100

    Working on the Login checks

--- InformationandTechnologyCollege/InformationandTechnologyCollege/Models/LoginDbModel.Designer.cs
+++ InformationandTechnologyCollege/InformationandTechnologyCollege/Models/LoginDbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\reega\Source\Repos\React_Teamv2\InformationandTechnologyCollege\InformationandTechnologyCollege\Models\DbModels.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\reega\Source\Repos\React_Teamv2\InformationandTechnologyCollege\InformationandTechnologyCollege\Models\LoginDbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- InformationandTechnologyCollege/InformationandTechnologyCollege/Services/Data/clsSecurityDAO.cs
+++ InformationandTechnologyCollege/InformationandTechnologyCollege/Services/Data/clsSecurityDAO.cs
@@ -2,6 +2,7 @@
 using  InformationandTechnologyCollege.Services.Business;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -10,31 +11,56 @@ namespace InformationandTechnologyCollege.Services.Data
     public class clsSecurityDAO
     {
         // connect to the databasse
-        string connectionString = "Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=App_Data\aspnet-InformationandTechnologyCollege-20220217094051.mdf;Initial Catalog=aspnet-InformationandTechnologyCollege-20220217094051;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LoginDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         internal bool FindByUser(clsUserModel username)
         {
-
+            // nothing was found in this query
             bool success = false;
+
+            //sql expression
+            string queryString = "SELECT * FROM dbo.tblLogin WHERE Email = @Email AND Password = @Password";
+
+            // creates and open the connection to the Db inside using a block
+            //this makes sure that all resources are closed correctly when the query is finished
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //Command and parameter objects
+                SqlCommand command = new SqlCommand(queryString, connection);
+                // linking @Email to .Email
+                command.Parameters.Add("@Email", System.Data.SqlDbType.NVarChar, 128).Value = username.Email;
+                command.Parameters.Add("@Password", System.Data.SqlDbType.NVarChar, int.MaxValue).Value = username.Password;
+
+                //open the Db and run commands
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        success = true;
+
+                    }
+                    else
+                    {
+                        success = false;
+                    }
+                    reader.Close();
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+
+                }
+                return success;
+            }
+
 
             //clsSecurityService securityService = new clsSecurityService();
             //{
-
-            //    //if (username.Email == "Admin@gmail.com" && username.Password == "Qwert123@")
-            //    //{
-
-            //    //    return true;
-
-            //    //}
-            //    //else
-            //    //{
-
-            //    //    return false;
-
-            //    //}
-            //    ///^^^^^old version of what is below, new code is cleaner but im keeping the old one in as both an example or if somthing breaks
             //    //return (username.Email == "Admin@gmail.com" && username.Password == "Qwerty123@");
-            //    ////////////////code above is a hardcoded login, great for testing pages work, terrible for a live site//////////////////////////
-
+            //    //////code above is a hardcoded login, great for testing pages work, terrible for a live site//////
             //}
         }
     }
