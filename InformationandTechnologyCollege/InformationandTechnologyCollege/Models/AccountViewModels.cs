f294187148f43ae595e28e510e8769f1f3ba395e
Reegan Hodges <P2598200@my365.dmu.ac.uk>
2022-04-15 12:39:37 +0100

    Finished

--- InformationandTechnologyCollege/InformationandTechnologyCollege/Models/AccountViewModels.cs
+++ InformationandTechnologyCollege/InformationandTechnologyCollege/Models/AccountViewModels.cs
@@ -71,11 +71,11 @@ namespace InformationandTechnologyCollege.Models
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        //[DataType(DataType.Password)]
+        [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        //[DataType(DataType.Password)]
+        [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
@@ -90,11 +90,11 @@ namespace InformationandTechnologyCollege.Models
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        //[DataType(DataType.Password)]
+        [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        //[DataType(DataType.Password)]
+        [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
